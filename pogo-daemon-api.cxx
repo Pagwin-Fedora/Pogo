0b3200f2f3b6e31641701afa7eb5a8a4b8e89aee
Pagwin <git@pagwin.xyz>
2021-09-06 16:46:58 -0400

    began implementing code and added a GPL 3 license because why not

--- pogo-daemon-api.cxx
+++ pogo-daemon-api.cxx
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <queue>
 #include <string>
 #include <functional>
@@ -5,17 +6,64 @@
 using std::string;
 
 
-class Message{
+class UpdateMessage{
 
 };
+class QueryMessage{
 
-class StateUpdate{};
+};
+class StateUpdate{
+	string toString(){
+		return "";
+	}
+};
+class QueryResult{
+	string toString(){
+		return "";
+	}
+};
+union _Message{
+	UpdateMessage update;
+	QueryMessage query;
+};
+class Message{
+	public:
+	std::uint32_t kind;
+	_Message value;
+	const static std::uint32_t updateKind;
+	const static std::uint32_t queryKind;
+};
+const uint32_t Message::updateKind = 0;
+const uint32_t Message::queryKind = 1;
 
-class StateQueryResult{};
+std::function<void(string)> returnContact;
+std::function<StateUpdate(UpdateMessage)> updateAddress;
+std::function<QueryResult(QueryMessage)> queryAddress;
 
-std::function<string(string)> contactExchange(std::function<void(string)>){
-	
+string contact(string message){
+	Message toForward = parseMessage(message);
+	switch(toForward.kind){
+		case Message::updateKind:
+			return updateAddress(toForward.value.update).toString();
+			break;
+		case Message::queryKind:
+			return queryAddress(toForward.value.query).toString();
+			break;
+		default:
+			throw "bad kind specifier";
+	}
 }
-std::function<void(StateUpdate)> addressExchange(std::function<StateUpdate(Message)>){
 
+void address(StateUpdate update){
+	returnContact(update.toString())
+}
+
+std::function<string(string)> contactExchange(std::function<void(string)> ret){
+	returnContact = ret;
+	return contact;
+}
+std::function<void(StateUpdate)> addressExchange(std::function<StateUpdate(UpdateMessage)> up,std::function<QueryResult(QueryMessage)> que){
+	updateAddress = up;
+	queryAddress = que;
+	return address;
 }
