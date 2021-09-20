9a77e8cb9a062ef94dab2e01e7d40eb7a792d63f
Pagwin <git@pagwin.xyz>
2021-09-20 18:06:01 -0400

    debugging rust code no idea of C++ is even running it yet lol

--- tcp_frontend/src/lib.rs
+++ tcp_frontend/src/lib.rs
@@ -3,13 +3,14 @@ use libc::size_t;
 use libc::c_uchar;
 
 extern{
-    fn contactExchange(contactPoint:&dyn FnMut(&str)-> ()) -> &dyn FnMut(&str) -> &str;
+    fn contactExchange(contactPoint:&dyn FnMut(&mut str)-> ()) -> &dyn FnMut(&mut str) -> &str;
 }
 fn init_frontend(len: size_t, config_buffer:&[c_uchar]){
     println!("pog!");
+    let mut s:String = String::from("Hello From rust!");
     unsafe{
-        println!("received from C++: {}",contactExchange(|stri: &str|{
+        println!("received from C++: {}", contactExchange(&mut move |stri: &mut str|{
             println!("called from C++ with {}", stri);
-        })());
+        })(s.as_mut_str()));
     }
 }
