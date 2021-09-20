d7e4c7367ea975e0e8567c2315313e4a67c17229
Pagwin <git@pagwin.xyz>
2021-09-20 17:34:06 -0400

    a bunch of progress on the rust stuff I don't wanna lose

--- pogo-daemon.cxx
+++ pogo-daemon.cxx
@@ -1,1 +1,6 @@
-int main(){};
+#include "pogo-daemon-api.h"
+#include <iostream>
+int main(){
+	std::cout << "this may go badly" << std::endl;
+	init_frontend(0, (unsigned char*)"");
+}
--- tcp_frontend/src/lib.rs
+++ tcp_frontend/src/lib.rs
@@ -3,8 +3,13 @@ use libc::size_t;
 use libc::c_uchar;
 
 extern{
-    fn
+    fn contactExchange(contactPoint:&dyn FnMut(&str)-> ()) -> &dyn FnMut(&str) -> &str;
 }
-fn init_frontend(len: size_t, buffer:&[c_uchar]){
-
+fn init_frontend(len: size_t, config_buffer:&[c_uchar]){
+    println!("pog!");
+    unsafe{
+        println!("received from C++: {}",contactExchange(|stri: &str|{
+            println!("called from C++ with {}", stri);
+        })());
+    }
 }
