4f38ce77dfb977634ca360b843303c5d18699590
Pagwin <git@pagwin.xyz>
2021-09-22 18:14:46 -0400

    I changed stuff but I can't figure out how to have contact Exchange work

--- tcp_frontend/src/lib.rs
+++ tcp_frontend/src/lib.rs
@@ -1,16 +1,17 @@
 extern crate libc;
 use libc::size_t;
 use libc::c_uchar;
-
 extern{
-    fn contactExchange(contactPoint:&dyn FnMut(&mut str)-> ()) -> &dyn FnMut(&mut str) -> &str;
+    fn contactExchange(contactPoint:&dyn Fn(&str)-> ()) -> &dyn FnMut(&mut str) -> &str;
 }
-fn init_frontend(len: size_t, config_buffer:&[c_uchar]){
+fn init_frontend(_len: size_t, _config_buffer:&[c_uchar]){
     println!("pog!");
     let mut s:String = String::from("Hello From rust!");
+    let mut closure = |stri: &str|{
+                    println!("called from C++ with {}", stri);
+                };
     unsafe{
-        println!("received from C++: {}", contactExchange(&mut move |stri: &mut str|{
-            println!("called from C++ with {}", stri);
-        })(s.as_mut_str()));
+        println!("received from C++: {}", contactExchange(&closure
+        )(s.as_mut_str()));
     }
 }
