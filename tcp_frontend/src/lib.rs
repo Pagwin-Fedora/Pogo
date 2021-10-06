575d2544b5b618e9a26f4dd7b8da9b6384f06f7a
Pagwin <git@pagwin.xyz>
2021-10-06 18:02:45 -0400

    started tcp implementation once it's done documentation on how this all works needs to be redone

--- tcp_frontend/src/lib.rs
+++ tcp_frontend/src/lib.rs
@@ -3,28 +3,60 @@ extern crate libc;
 use libc::c_char;
 use libc::c_uchar;
 use libc::size_t;
+use std::default::Default;
 use std::ffi::{CStr,CString};
+use std::io::prelude::*;
+use std::net::{TcpStream,TcpListener};
 extern{
     fn StringForward(data:*mut c_char) -> *mut c_char;
 }
 #[repr(C)]
-pub struct FrontendState{
-    
+#[derive(Clone, Copy)]
+pub struct ConfigInfo{
+    port:u32
 }
-impl std::default::Default for FrontendState {
+impl Default for ConfigInfo{
     fn default() -> Self {
-        return FrontendState{}   
+        ConfigInfo{
+            //change default port
+            port:1337
+        }
     }
+}
+impl From<*const c_uchar> for ConfigInfo {
+    fn from(bytes:*const c_uchar)->Self{
+        //TODO: actually implement from here
+        Self::default()
+    }
+}
+#[repr(C)]
+#[derive(Default)]
+pub struct FrontendState{
+    conns:Vec<TcpStream>,
+    config:ConfigInfo
 }
 #[no_mangle]
 pub extern fn initFrontend(_len: size_t, config_buffer:*const c_uchar)-> *mut FrontendState{
-    let a = CString::new("some message to C").unwrap().into_raw();
-    unsafe {
-        StringForward(a);
-        println!("{}",CStr::from_ptr(config_buffer as *const i8).to_str().unwrap());
-    }
-    let state:Box<FrontendState> = Box::default();
-    //hmmm
+    let mut state:Box<FrontendState> = Box::default();
+    state.config = ConfigInfo::from(config_buffer);
+    //somehow I need to do stuff with state here with thread safety and I need to return state when
+    //I'm done with this function, fuck
+    std::thread::spawn(||{
+        let listener = TcpListener::bind(format!("127.0.0.1:{}",state.config.port)).unwrap();
+        for result in listener.incoming(){
+            match result {
+                Ok(conn) => {
+                    negotiate(&mut conn);
+                    state.conns.push(conn);
+                },
+                Err(_) => continue
+            }
+
+        }
+    });
+
+
+
     return Box::<FrontendState>::into_raw(state);
 }
 #[allow(non_snake_case)]
@@ -37,8 +69,6 @@ pub unsafe extern fn StringBack(_state:*mut FrontendState,stri: *const c_char){
 pub unsafe extern fn MessageReceive(_msg_ptr:*mut ()){
     println!("Message received but we don't care");
 }
-//#[panic_handler]
-//fn dont_panic(x:&PanicInfo) -> !{
-//    loop{}
-//}
-//#[lang = "eh_personality"] extern fn rust_eh_personality() {}
+fn negotiate(conn:&mut TcpStream){
+    
+}
