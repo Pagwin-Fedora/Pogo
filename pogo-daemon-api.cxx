199496e3c93c6951b5c05921d5344a1029998b6c
Pagwin <git@pagwin.xyz>
2021-11-06 19:28:26 -0400

    laid a lot of the groundwork to write up the postgresql backend for this

--- pogo-daemon-api.cxx
+++ pogo-daemon-api.cxx
@@ -34,6 +34,7 @@ void initMiddleware(void *f_state, void *b_state){
 	backend_state = b_state;
 	while(true) std::this_thread::sleep_for(std::chrono::minutes(1));
 }
+//both of these do nothing because I don't need them to do anything right now(I just realized I should probably put them as another library that gets linked in rather than in the source files themselves but at this point fuck it I'll do it when I refactor this into more git repos)
 char* BackToFront(char* resp,size_t size,size_t *newSize){
 	*newSize = size;
 	return resp;
@@ -42,4 +43,10 @@ char* BackToFront(char* resp,size_t size,size_t *newSize){
 char* FrontToBack(char* resp,size_t size,size_t *newSize){
 	*newSize = size;
 	return resp;
+}
+
+void StateUpdate(char* update, size_t updateLen){
+	//new size_t var declared to avoid potential bugs where the new size is updated but the old size is still needed
+	size_t newUpdateLen;
+	update = BackToFront(update, updateLen, &newUpdateLen);
 }
--- postgres_backend/postgres_backend.go
+++ postgres_backend/postgres_backend.go
@@ -5,15 +5,18 @@ import (
 	"sync"
 	"encoding/json"
 	"database/sql"
-	"github.com/lib/pq"
 	"unsafe"
+	_ "github.com/jackc/pgx/v4/stdlib"
 );
 
 //#include<stdlib.h>
+//#include "../pogo-daemon-api.h"
 import "C";
 
 type BackendState struct {
+	mutex sync.Mutex
 	config BackendConfig
+	db *sql.DB
 }
 type BackendConfig struct {
 	host string `json:dbHost`
@@ -23,20 +26,28 @@ type BackendConfig struct {
 	name string `json:dbName`
 }
 //export initBackend
-func initBackend(size C.size_t, config *C.char){
+func initBackend(size C.size_t, rawConfig *C.char) *C.void{
 	//this is scary
 	//goConfig = C.GoString(config)
 	var config BackendConfig
-	byte_array := (*[size]byte)(unsafe.Pointer(config))
+	//I think this is fine but I'm not sure lol
+	byte_array := (*(*[]byte)(unsafe.Pointer(rawConfig)))[:size]
+
 	json.Unmarshal(byte_array,&config)
-	//sslmode=disable worries me
-	dbInfo = fmt.SprintF("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",config.dbHost,config.dbPort,config.dbUser,config.dbPassword,config.dbName)
-
+	URL := fmt.Sprintf("postgres://%s:%s@%s:%d/%s",config.user,config.password,config.host,config.port,config.name)
+	db, err := sql.Open("pgx", URL)
+	if err != nil {
+		fmt.Println("oh no backend didn't initialize don't know how to actually deal with this so a panic is probably imminent somewhere")
+	}
+	if db.Ping() != nil {
+		fmt.Println("ping failed")
+	}
+	return (*C.void)(unsafe.Pointer(&BackendState {sync.Mutex{},config,db}))
 }
+type 
 //export MessageReceive
-func MessageReceive(){
+func MessageReceive(state *C.void, message *C.char, messageLen C.size_t, returnMessageLen) *C.char{
 
-	fmt.Println("ono");
 }
 //why do you have to do this to me go
 func main(){}
